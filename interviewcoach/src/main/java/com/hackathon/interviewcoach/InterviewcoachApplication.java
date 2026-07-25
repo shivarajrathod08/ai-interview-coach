c252c13113c4234799e5997fc4e1b29e4f1fe94f
Shivarajrathod08 <shivarajrathod12345@gmail.com>
2026-07-25 10:16:42 +0530

    Initial Spring Boot project setup

---
+++ interviewcoach/src/main/java/com/hackathon/interviewcoach/InterviewcoachApplication.java
@@ -0,0 +1,13 @@
+package com.hackathon.interviewcoach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewcoachApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewcoachApplication.class, args);
+    }
+
+}
