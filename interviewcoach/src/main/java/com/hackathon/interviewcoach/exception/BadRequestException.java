28701fb419e7ad8ece71e3479bcafb27fd70db8f
Rathlavath Anil <anilrathlavath77@gmail.com>
2026-07-25 11:39:17 +0530

    Added exception classes

--- interviewcoach/src/main/java/com/hackathon/interviewcoach/exception/BadRequestException.java
+++ interviewcoach/src/main/java/com/hackathon/interviewcoach/exception/BadRequestException.java
@@ -1,4 +1,8 @@
 package com.hackathon.interviewcoach.exception;
 
-public class BadRequestException {
+public class BadRequestException extends RuntimeException {
+
+    public BadRequestException(String message) {
+        super(message);
+    }
 }
