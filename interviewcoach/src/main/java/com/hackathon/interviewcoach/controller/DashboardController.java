0764a422e2491de69f85d05a2f9cfe702c6b0780
Shivarajrathod08 <shivarajrathod12345@gmail.com>
2026-07-25 12:11:37 +0530

    Add DashboardController

--- interviewcoach/src/main/java/com/hackathon/interviewcoach/controller/DashboardController.java
+++ interviewcoach/src/main/java/com/hackathon/interviewcoach/controller/DashboardController.java
@@ -1,4 +1,26 @@
 package com.hackathon.interviewcoach.controller;
 
+import com.hackathon.interviewcoach.dto.response.DashboardResponse;
+import com.hackathon.interviewcoach.service.DashboardService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/dashboard")
+@RequiredArgsConstructor
+@Tag(name = "Dashboard", description = "Endpoints for aggregated user analytics")
 public class DashboardController {
+
+    private final DashboardService dashboardService;
+
+    @GetMapping
+    @Operation(summary = "Get dashboard analytics for the current user")
+    public ResponseEntity<DashboardResponse> getDashboard() {
+        return ResponseEntity.ok(dashboardService.getDashboard());
+    }
 }
