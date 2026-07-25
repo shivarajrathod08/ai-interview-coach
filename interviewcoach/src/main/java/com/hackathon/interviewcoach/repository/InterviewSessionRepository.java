9bf491b42915a1d0c581e90611d2cddaa81b70fc
Rathlavath Anil <anilrathlavath77@gmail.com>
2026-07-25 11:59:50 +0530

    Added Interview Repository

--- interviewcoach/src/main/java/com/hackathon/interviewcoach/repository/InterviewSessionRepository.java
+++ interviewcoach/src/main/java/com/hackathon/interviewcoach/repository/InterviewSessionRepository.java
@@ -1,4 +1,17 @@
 package com.hackathon.interviewcoach.repository;
 
-public class InterviewSessionRepository {
+import com.hackathon.interviewcoach.entity.InterviewSession;
+import com.hackathon.interviewcoach.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface InterviewSessionRepository extends JpaRepository<InterviewSession, Long> {
+
+    List<InterviewSession> findByUserOrderByCreatedAtDesc(User user);
+
+    Optional<InterviewSession> findByIdAndUser(Long id, User user);
+
+    long countByUser(User user);
 }
